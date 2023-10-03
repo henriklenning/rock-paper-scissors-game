7404b610aa4f950c591670388c5d0c8cd08f5d6a
henriklenning <henrik.fohrenbachlenning@gmail.com>
2023-10-04 00:33:12 +0200

    Add a function to play one round The function takes the input from the player and compares it to the random generated choice of the computer.

--- index.js
+++ index.js
@@ -9,10 +9,66 @@ function getComputerChoice() {
   }
 }
 
+// function playRound(playerSelection, computerSelection) {
+//   if (playerSelection === "ROCK" && computerSelection === "Rock") {
+//     console.log("It's a tie!");
+//   }
+//   if (playerSelection === "ROCK" && computerSelection === "Paper") {
+//     console.log("Player lost!");
+//   }
+//   if (playerSelection === "ROCK" && computerSelection === "Scissor") {
+//     console.log("Player wins!");
+//   }
+//   if (playerSelection === "PAPER" && computerSelection === "Rock") {
+//     console.log("Player wins!");
+//   }
+//   if (playerSelection === "PAPER" && computerSelection === "Paper") {
+//     console.log("It's a tie!");
+//   }
+//   if (playerSelection === "PAPER" && computerSelection === "Scissor") {
+//     console.log("Player lost!");
+//   }
+//   if (playerSelection === "SCISSORS" && computerSelection === "Rock") {
+//     console.log("Player lost!");
+//   }
+//   if (playerSelection === "SCISSORS" && computerSelection === "Paper") {
+//     console.log("Player wins!");
+//   }
+//   if (playerSelection === "SCISSORS" && computerSelection === "Scissor") {
+//     console.log("It's a tie!");
+//   }
+// }
 
+function playRound(playerSelection, computerSelection) {
+  if (
+    (computerSelection === "Rock" && playerSelection === "ROCK") ||
+    (computerSelection === "Paper" && playerSelection === "PAPER") ||
+    (computerSelection === "Scissors" && playerSelection === "SCISSORS")
+  ) {
+    return "A tie!";
+  }
+  if (
+    (computerSelection === "Rock" && playerSelection === "PAPER") ||
+    (computerSelection === "Paper" && playerSelection === "SCISSORS") ||
+    (computerSelection === "Scissors" && playerSelection === "ROCK")
+  ) {
+    return "You loose!";
+  }
+  if (
+    (computerSelection === "Rock" && playerSelection === "SCISSORS") ||
+    (computerSelection === "Paper" && playerSelection === "ROCK") ||
+    (computerSelection === "Scissors" && playerSelection === "PAPER")
+  ) {
+    return "You win!";
+  }
+}
 
+const playerSelection = "SCISSORS";
+const computerSelection = getComputerChoice();
 
+console.log(playRound(playerSelection, computerSelection));
 
+// console.log(playRound(playerSelection, computerSelection));
 
 // I need a function that chooses between roch paper scissors for the computer
 // Then compare computers choise and user choice and declare winner based on result of comparison
